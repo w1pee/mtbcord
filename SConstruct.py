ddaa493bb6b4df4c9f60a1b3f345edbaf68089a4
Motoko <meikoprivat@icloud.com>
2023-07-18 23:32:40 +0200

    Created default source structure.

---
+++ SConstruct.py
@@ -0,0 +1,7 @@
+import os
+
+env = Environment(CC='gcc', CCFLAGS='-std=c99 -O3 -ftree-vectorize -Werror -Wall -Wextra -Wfloat-equal -Wundef -Wshadow -Wcast-align -Wstrict-prototypes -Wswitch-default -Wswitch-enum -Wunreachable-code -Wformat=2')
+
+env.StaticLibrary('http', Glob('src/http/*.c'))
+env.StaticLibrary('api', Glob('src/api/*.c'))
+env.Program('mtbcord',Glob('src/*.c'), LIBS=['http', 'api'], LIBPATH='.', CPPPATH=['src/http/', 'src/api/']) 
