4586e222fc1ba4975865ae10893b0dd5e44be39e
Motoko <meikoprivat@icloud.com>
2023-07-19 18:01:34 +0200

    + termbox2 + License in main.c

--- SConstruct
+++ SConstruct
@@ -1,7 +1,8 @@
 import os
 
-env = Environment(CC='gcc', CCFLAGS='-std=c99 -O3 -ftree-vectorize -Werror -Wall -Wextra -Wfloat-equal -Wundef -Wshadow -Wcast-align -Wstrict-prototypes -Wswitch-default -Wswitch-enum -Wunreachable-code -Wformat=2')
+env = Environment(CC='gcc', CCFLAGS='-std=c99 -O3 -ftree-vectorize -Werror -Wall -Wextra -Wfloat-equal -Wundef -Wshadow -Wcast-align -Wstrict-prototypes -Wswitch-enum -Wunreachable-code -Wformat=2')
+
 
 env.StaticLibrary('http', Glob('src/http/*.c'))
 env.StaticLibrary('api', Glob('src/api/*.c'))
-env.Program('mtbcord',Glob('src/*.c'), LIBS=['http', 'api'], LIBPATH='.', CPPPATH=['src/http/', 'src/api/']) 
+env.Program('mtbcord',Glob('src/*.c') + ['deps/termbox2/termbox2.h'], LIBS=['http', 'api'], LIBPATH='.', CPPPATH=['src/http/', 'src/api/', 'deps/termbox2/']) 
--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
+// mtbcord a terminal based discord client
+// Copyright (C) 2023 Motoko Kusanagi (https://github.com/MotokoKusanagii)
+// 
+//                   GNU GENERAL PUBLIC LICENSE
+//                     Version 3, 29 June 2007
+//
+// This program is free software; you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the Free 
+// Software Foundation; version 3 of the License 
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+// FITNESS FOR A PARTICULAR PURPOSE.    See the GNU General Public License for 
+// more details.
+//
+//
+#define TB_IMPL
+#include <termbox2.h>
 #include <stdio.h>
+
 
 int main(void)
 {
-  printf("Hello World!\n");
+  printf("Hello World\n");
   return 0;
 }
